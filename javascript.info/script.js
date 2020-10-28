32b3647a1d29e8934b3a18ae0d267b4f1628132a
BAEK-SEUNGYUN <bsy6953@gmail.com>
2020-10-28 22:55:39 +0900

    commit

--- javascript.info/script.js
+++ javascript.info/script.js
@@ -1,7 +1,12 @@
-alert('hello world');
+// alert('hello world');
 
-let admin, name;
-name = "John";
-admin = name;
+// let admin, name;
 
-alert(admin);
+// admin = prompt('입력해주세요.', ); //IE에서는 디폴트값을 넣어 줘야함 비어보이게 할라면'' 안그러면 undefined가 나옴
+
+// alert(admin);
+
+let name = prompt("이름을 입력하세요",);
+
+alert(name);
+
--- zerocho/script.js
+++ zerocho/script.js
@@ -1,37 +1,65 @@
-var relationship1 = {
-    name: 'zero',
-    friends: ['nero', 'hero', 'xero'],
-    logFriends: function(){
-        var that = this;
-        this.friends.forEach(function (friend){
-            console.log(that.name, friend); //console,log(this.namem friend)
-        });
-    },
-};
-relationship1.logFriends();
+// var relationship1 = {
+//     name: 'zero',
+//     friends: ['nero', 'hero', 'xero'],
+//     logFriends: function(){
+//         var that = this;
+//         this.friends.forEach(function (friend){
+//             console.log(that.name, friend); //console,log(this.namem friend)
+//         });
+//     },
+// };
+// relationship1.logFriends();
 
 
-const relationship2 = {
-    name: 'zero',
-    friends:['nero', 'hero', 'xero'],
-    logFriends(){
-        this.friends.forEach(friend =>{
-            console.log(this.name,friend);
-        });
-    },
-};
-relationship1.logFriends();
+// const relationship2 = {
+//     name: 'zero',
+//     friends:['nero', 'hero', 'xero'],
+//     logFriends(){
+//         this.friends.forEach(friend =>{
+//             console.log(this.name,friend);
+//         });
+//     },
+// };
+// relationship1.logFriends();
 
 
 
-const relationship3 = {
-    name: 'zero',
-    friends:['nero', 'hero', 'xero'],
-    logFriends(){
-        this.friends.forEach(friend =>{
-            console.log(this.name,friend);
-        });
-    },
-};
-relationship3.logFriends();
+// const relationship3 = {
+//     name: 'zero',
+//     friends:['nero', 'hero', 'xero'],
+//     logFriends(){
+//         this.friends.forEach(friend =>{
+//             console.log(this.name,friend);
+//         });
+//     },
+// };
+// relationship3.logFriends();
+
+///////////////////////////////////////////////////////////////
+
+//
+const example = { a: 123, b: {c: 135, d:146}}
+const a = example.a;
+const b = example.b;
+const c = example.b.c;
+const d = example.b.d;
+
+//구조분해문법
+const {a,b:{d}} = example;
+
+//
+arr = [1,2,3,4,5];
+const x = arr[0];
+const y = arr[1];
+const z = arr[4];
+//이것을
+const[x,y,,,z] = arr;
+
+alert(c);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
 
