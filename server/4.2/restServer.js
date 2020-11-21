35938a06bdc8644eb35225bbe1b37c113d049be8
BAEK-SEUNGYUN <bsy6953@gmail.com>
2020-11-21 19:47:21 +0900

    commit

---
+++ server/4.2/restServer.js
@@ -0,0 +1,17 @@
+const { fstat } = require('fs');
+const http = require('http');
+const fd = require('fs').promises;
+
+const users = {};   //데이터 저장용
+
+http.createServer(async (req, res)=>{
+    try {
+        if(req.url ==='/'){
+            const data = await fs.readFile('./restFront.html');
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            
+        }
+    } catch (err) {
+        
+    }
+})
